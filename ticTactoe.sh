fdf16ad633146e3ccb9bce532f895f8df77b9e2c
noothanprem <rutujatikhile@gmail.com>
2020-02-24 13:01:46 +0530

    Added: Player would know which letter is assigned

--- ticTactoe.sh
+++ ticTactoe.sh
@@ -16,5 +16,14 @@ echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
 echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
 echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
 }
-resetBoard
-displayBoard
+
+function assignLetter(){
+if (( $((RANDOM%2))==1 ))
+then
+	letter='X'
+else
+	letter='O'
+fi
+echo your letter is $letter
+}
+
