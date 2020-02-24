d8e6edb311caa3d887ceb22e341316b3ad203db3
noothanprem <rutujatikhile@gmail.com>
2020-02-24 12:56:27 +0530

    Added: Player would like to start fresh with resetting board

--- ticTactoe.sh
+++ ticTactoe.sh
@@ -2,3 +2,19 @@
 
 echo "Welcome to the game TIC-TAC-TOE"
 
+declare -A board
+
+resetBoard(){
+for((i=1;i<=9;i++))
+do
+	board[$i]=" " 
+done
+}
+
+displayBoard(){
+echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
+echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
+echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
+}
+resetBoard
+displayBoard
